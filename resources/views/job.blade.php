d9e8433b723a7a3b45fccc19579ca2fafcb0089a
Evan Rasheed <evaan0202@gmail.com>
2025-04-01 00:33:22 +0500

    learning db stuff

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -4,6 +4,6 @@
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
 
     <p class="text-gray-500">
-        This job pays{{ $job['salary'] }} per year.
+        This job pays {{ $job['salary'] }} per year.
     </p>
 </x-layout>
